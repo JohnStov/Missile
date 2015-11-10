1af99a153b1d4e17c8b7dec15cbba771b1a2f662
John Stovin <john.d.stovin@gmail.com>
2015-11-10 16:24:02 +0000

    Launcher works with Desktop & Pi

---
+++ Missile/Launcher.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.HumanInterfaceDevice;
+using Windows.Storage;
+
+namespace Missile
+{
+    public class Launcher
+    {
+        public static async Task<Launcher> CreateAsync()
+        {
+            var selector = HidDevice.GetDeviceSelector(0x01, 0x10, 0x2123, 0x1010);
+
+            var devices = await DeviceInformation.FindAllAsync(selector);
+            if (devices.Count > 0)
+            {
+                var device = await HidDevice.FromIdAsync(devices[0].Id, FileAccessMode.ReadWrite);
+                return new Launcher(device);
+            }
+
+            return null;
+        }
+
+        private HidDevice device;
+
+        private Launcher(HidDevice device)
+        {
+            this.device = device;
+        }
+
+        private async Task SendOutputMessage(byte[] message)
+        {
+            if (device != null)
+            {
+                var report = device.CreateOutputReport();
+                report.Data = message.AsBuffer();
+                await device.SendOutputReportAsync(report);
+            }
+        }
+
+        private static readonly byte[] CMD = { 0, 0, 0, 0, 0, 0, 0, 0, 2 };
+        private static readonly byte[] Up = { 0, 2, 2, 0, 0, 0, 0, 0, 0 };
+        private static readonly byte[] DOWN = { 0, 2, 1, 0, 0, 0, 0, 0, 0 };
+        private static readonly byte[] LEFT = { 0, 2, 4, 0, 0, 0, 0, 0, 0 };
+        private static readonly byte[] RIGHT = { 0, 2, 8, 0, 0, 0, 0, 0, 0 };
+        private static readonly byte[] FIRE = { 0, 2, 16, 0, 0, 0, 0, 0, 0 };
+        private static readonly byte[] Stop = { 0, 2, 32, 0, 0, 0, 0, 0, 0 };
+        private static readonly byte[] GetStatus = { 0, 1, 0, 0, 0, 0, 0, 0, 0 };
+        private static readonly byte[] LedOn = { 0, 3, 1, 0, 0, 0, 0, 0, 0 };
+        private static readonly byte[] LedOff = { 0, 3, 0, 0, 0, 0, 0, 0, 0 };
+        private const int delay = 1000;
+
+        public async Task SetLightOnAsync()
+        {
+            await SendOutputMessage(LedOn);
+        }
+
+        public async Task SetLightOffAsync()
+        {
+            await SendOutputMessage(LedOff);
+        }
+
+        public async Task MoveUpAsync()
+        {
+            await SendOutputMessage(Up);
+            await Task.Delay(delay);
+            await SendOutputMessage(Stop);
+        }
+
+        public async Task MoveDownAsync()
+        {
+            await SendOutputMessage(DOWN);
+            await Task.Delay(delay);
+            await SendOutputMessage(Stop);
+        }
+
+        public async Task MoveLeftAsync()
+        {
+            await SendOutputMessage(LEFT);
+            await Task.Delay(delay);
+            await SendOutputMessage(Stop);
+        }
+
+        public async Task MoveRightAsync()
+        {
+            await SendOutputMessage(RIGHT);
+            await Task.Delay(delay);
+            await SendOutputMessage(Stop);
+        }
+
+        public async Task FireAsync()
+        {
+            await SendOutputMessage(FIRE);
+        }
+    }
+}
--- Missile/MainPage.xaml.cs
+++ Missile/MainPage.xaml.cs
@@ -1,4 +1,10 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.HumanInterfaceDevice;
+using Windows.Devices.Sensors;
+using Windows.Storage;
+using Windows.UI.Xaml.Controls;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -12,6 +18,52 @@ namespace Missile
         public MainPage()
         {
             InitializeComponent();
+            Loaded += MainPage_Loaded;
+        }
+
+        private Launcher launcher;
+
+        private void MainPage_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            launcher = Task.Run(Launcher.CreateAsync).Result;
+            Task.Run(launcher.SetLightOnAsync);
+        }
+
+        private void Up_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            Task.Run(launcher.MoveUpAsync);
+        }
+
+        private void Left_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            Task.Run(launcher.MoveLeftAsync);
+        }
+
+        private void Fire_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            Task.Run(launcher.FireAsync);
+        }
+
+        private void Right_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            Task.Run(launcher.MoveRightAsync);
+        }
+
+        private void Down_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            Task.Run(launcher.MoveDownAsync);
+        }
+
+        private void Led_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            if (launcher == null)
+                return;
+
+            var checkbox = sender as CheckBox;
+            if (checkbox.IsChecked.Value)
+                Task.Run(launcher.SetLightOnAsync);
+            else
+                Task.Run(launcher.SetLightOffAsync);
         }
     }
 }
